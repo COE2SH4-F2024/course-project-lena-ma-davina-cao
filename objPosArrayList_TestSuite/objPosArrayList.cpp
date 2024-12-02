d267c72484f5fc9df366620cbb68084427287894
mal55-lenama <mal55@mcmaster.ca>
2024-12-02 02:39:25 -0500

    objPosArrayList (2A), beginning of 2B (food class)

--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -5,9 +5,9 @@
 
 objPosArrayList::objPosArrayList()
 {
-    objPos* aList;
-    listSize = 1;
-    arrayCapacity = 1; //dcheck
+    listSize = 0;
+    arrayCapacity = 200; //dcheck
+    aList = new objPos[arrayCapacity];
 }
 
 objPosArrayList::~objPosArrayList()
@@ -29,47 +29,61 @@ void objPosArrayList::insertHead(objPos thisPos)
 {
     listSize++;
 
-    for (int i = 0; i < listSize; i++){
-        objPos temp(aList[i+1]);
-        if(i == 0){
+    for (int i = listSize - 1; i >= 0; i--){
+        if (i > 0){
+            aList[i].pos->x = aList[i-1].pos->x;
+            aList[i].pos->y = aList[i-1].pos->y;
+            aList[i].symbol = aList[i-1].symbol;
+        }
+        else if (i == 0){
             aList[i].pos->x = thisPos.pos->x;
             aList[i].pos->y = thisPos.pos->y;
-            aList[i].symbol = thisPos.symbol;
-        }
-        else {
-            aList[i].pos->x = temp.pos->x;
-            aList[i].pos->y = temp.pos->y;
-            aList[i].symbol = temp.symbol;
+            aList[i].symbol = thisPos.symbol;                
         }
     }
 }
 
+
 void objPosArrayList::insertTail(objPos thisPos)
 {
     
+    ++listSize;
+
+    aList[listSize - 1].pos->x = thisPos.pos->x;
+    aList[listSize - 1].pos->y = thisPos.pos->y;
+    aList[listSize - 1].symbol = thisPos.symbol;
 }
 
 void objPosArrayList::removeHead()
 {
-    
+    if (listSize > 0){
+        listSize--;
+    }
+    for (int i = 0; i < listSize - 1; i++){
+        aList[i].pos->x = aList[i+1].pos->x;
+        aList[i].pos->y = aList[i+1].pos->y;
+        aList[i].symbol = aList[i+1].symbol;
+    }
 }
 
 void objPosArrayList::removeTail()
 {
-    
+    if (listSize != 0){
+        listSize--;
+    }
 }
 
 objPos objPosArrayList::getHeadElement() const
 {
-    
+    return objPos(aList[0]);
 }
 
 objPos objPosArrayList::getTailElement() const
 {
-    
+    return objPos(aList[listSize - 1]);
 }
 
 objPos objPosArrayList::getElement(int index) const
 {
-    
+    return objPos(aList[index]);
 }
